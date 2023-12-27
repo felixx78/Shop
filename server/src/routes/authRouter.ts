cb7a778e5d5e451fe8104238d56dd9dd0bfa8ef6
v1st78 <petrenkov605@gmail.com>
2023-12-27 14:57:00 +0200

    added /login and /refresh

--- server/src/routes/authRouter.ts
+++ server/src/routes/authRouter.ts
@@ -2,6 +2,8 @@ import { Request, Response, Router } from "express";
 import query from "../utils/db";
 import hashPassword from "../utils/hashPassword";
 import { User } from "../types/user";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import {
   generateAccessToken,
   generateRefreshToken,
@@ -69,7 +71,80 @@ const authRouter = Router()
       return res.status(500);
     }
   })
-  .post("/login")
-  .get("/refresh");
+  .post("/login", async (req: Request, res: Response) => {
+    try {
+      const { email, password } = req.body;
+
+      const userFind = await query("SELECT * from users where email = ?", [
+        email,
+      ]);
+
+      if (Array.isArray(userFind)) {
+        if (userFind.length === 0) {
+          return res.status(401).json("User does not exist");
+        }
+        const user = userFind[0] as User;
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+          return res.status(401).json("Incorrect password");
+        }
+
+        const payload = {
+          id: user.id,
+          email: user.email,
+          role: user.role,
+        };
+
+        const refreshToken = generateRefreshToken(payload);
+        const accessToken = generateAccessToken(payload);
+
+        const expirationDate = new Date();
+        expirationDate.setDate(expirationDate.getDate() + 30);
+
+        res.cookie("refreshToken", refreshToken, {
+          httpOnly: true,
+          expires: expirationDate,
+        });
+
+        return res.json({
+          accessToken,
+          payload,
+        });
+      }
+
+      return res.status(500);
+    } catch (e) {
+      console.log(e);
+      return res.status(500);
+    }
+  })
+  .get("/refresh", (req: Request, res: Response) => {
+    try {
+      const refreshToken = req.cookies?.refreshToken;
+
+      if (!refreshToken) {
+        return res.status(401).json("No refresh token provided");
+      }
+
+      jwt.verify(
+        refreshToken,
+        process.env.JWT_SECRET!,
+        (err: any, decoded: any) => {
+          if (err) return res.status(401).json("Invalid refresh token");
+
+          delete decoded.iat;
+          delete decoded.exp;
+
+          const accessToken = generateAccessToken(decoded);
+          return res.json(accessToken);
+        }
+      );
+    } catch (e) {
+      console.log(e);
+      return res.status(500);
+    }
+  });
 
 export default authRouter;
