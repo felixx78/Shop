3ea5595c85af6c2950e0aa8b61ca61cb62df09db
v1st78 <petrenkov605@gmail.com>
2023-12-27 15:07:58 +0200

    added user type to client/src/lib

--- client/src/lib/definition.ts
+++ client/src/lib/definition.ts
@@ -2,4 +2,14 @@ export type RootState = {
   theme: {
     theme: "dark" | "light";
   };
+  user: {
+    acessToken: string;
+    user: User;
+  };
+};
+
+export type User = {
+  id: number;
+  email: string;
+  role: string;
 };
