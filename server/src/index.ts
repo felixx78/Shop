48075f3a4f841a9bba650c40e400b8c36d1c9c28
v1st78 <petrenkov605@gmail.com>
2023-12-28 12:55:17 +0200

    added cloudinary

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,13 @@ import cors from "cors";
 import apiRouter from "./routes";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app: Application = express();
 const port = 3000;
