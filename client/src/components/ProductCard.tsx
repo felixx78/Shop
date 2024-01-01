ab88957239601618814a8f3cbe8987f362813461
v1st78 <petrenkov605@gmail.com>
2024-01-01 21:05:14 +0200

    add ProductCardSkeleton and min-h to img

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 import RatingStars from "./RatingStars";
 import { Product } from "../lib/definition";
+import Skeleton from "react-loading-skeleton";
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
     <div className="mx-auto max-w-[300px] bg-foreground pt-4 dark:bg-dark-foreground">
       <Link to={`/product/${product.id}`} className="mb-4 block px-4">
-        <img src={product.image} className="mb-2 text-center" alt="" />
+        <img
+          src={product.image}
+          className="mb-2 min-h-[200px] text-center"
+          alt=""
+        />
 
         <p className="mb-2 truncate text-lg font-bold" title={product.title}>
           {product.title}
@@ -21,6 +26,29 @@ const ProductCard = ({ product }: { product: Product }) => {
       <button className="block w-full bg-primary py-2 text-lg text-dark-copy hover:bg-primary-dark">
         Add to cart
       </button>
+    </div>
+  );
+};
+
+export const ProductCardSkeleton = () => {
+  return (
+    <div className="mx-auto w-[290px] bg-foreground pt-4 dark:bg-dark-foreground">
+      <div className="mb-4 px-4">
+        {/* image */}
+        <Skeleton height={350} className="mb-2 w-full" />
+
+        {/* title */}
+        <Skeleton width={140} height={25} className="mb-2" />
+
+        {/* price and stars */}
+        <div className="flex items-center justify-between">
+          <Skeleton width={80} height={20} />
+          <Skeleton width={100} height={20} />
+        </div>
+      </div>
+
+      {/* button  */}
+      <Skeleton height={40} className="w-full" />
     </div>
   );
 };
