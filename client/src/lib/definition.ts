b207d23c5b6ac557f829daa8e83ec30b6c715e1f
v1st78 <petrenkov605@gmail.com>
2024-01-01 17:10:16 +0200

    add Product type

--- client/src/lib/definition.ts
+++ client/src/lib/definition.ts
@@ -12,3 +12,16 @@ export type User = {
   email: string;
   role: string;
 };
+
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+};
