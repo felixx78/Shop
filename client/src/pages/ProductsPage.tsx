06f2109069747a20217857e9763b827a23d15714
v1st78 <petrenkov605@gmail.com>
2024-01-01 21:12:59 +0200

    ProductPage changes

--- client/src/pages/ProductsPage.tsx
+++ client/src/pages/ProductsPage.tsx
@@ -1,48 +1,110 @@
-import { Link } from "react-router-dom";
-
-import tshirt from "../assets/t-shirt-image.jpg";
-import manga from "../assets/manga-image.jpg";
-import toy from "../assets/toy-image.webp";
+import { useQuery } from "@tanstack/react-query";
+import { fetchAllCategories, fetchProducts } from "../api/product";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import { useEffect, useRef, useState } from "react";
+import ProductCard, { ProductCardSkeleton } from "../components/ProductCard";
 
 function ProductsPage() {
-  const products = [
-    {
-      name: "T-shirts",
-      link: "t-shirts",
-      image: tshirt,
-    },
-    {
-      name: "Manga",
-      link: "manga",
-      image: manga,
-    },
-    {
-      name: "Toys",
-      link: "toys",
-      image: toy,
-    },
-  ];
+  const [category, setCategory] = useState("");
+
+  const { data: products, isLoading } = useQuery({
+    queryKey: ["products", category],
+    queryFn: fetchProducts,
+  });
+
+  const { data: categories } = useQuery({
+    queryKey: ["categories"],
+    queryFn: fetchAllCategories,
+    initialData: [],
+  });
 
   return (
-    <div className="container pt-2">
-      <h1 className="mb-4 text-2xl font-bold">Products</h1>
-      <div className="grid grid-cols-1 items-start justify-items-center gap-4 sm:grid-cols-2 md:grid-cols-3">
-        {products.map((product) => (
-          <Link
-            to={product.link}
-            key={product.name}
-            className="block h-[300px] w-full bg-foreground lg:h-[400px] dark:bg-dark-foreground"
+    <div className="pt-2">
+      <div className="mb-4 flex flex-wrap justify-between px-8">
+        <h1 className="mb-4 text-2xl font-bold">Products</h1>
+        <SelectCategory
+          value={category}
+          onChange={setCategory}
+          categories={categories}
+        />
+      </div>
+
+      <div className="mx-auto max-w-[1220px]">
+        {isLoading || !products ? (
+          <div className="flex flex-wrap gap-2">
+            {Array.from({ length: 8 }, (_, i) => i).map((i) => (
+              <ProductCardSkeleton key={i} />
+            ))}
+          </div>
+        ) : (
+          <ResponsiveMasonry
+            columnsCountBreakPoints={{ 0: 1, 620: 2, 920: 3, 1220: 4 }}
           >
-            <img
-              src={product.image}
-              className="h-[260px] w-full object-cover lg:h-[360px]"
-              alt=""
-            />
-            <h2 className="py-2 text-center text-lg">{product.name}</h2>
-          </Link>
-        ))}
+            <Masonry gutter="25px">
+              {products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))}
+            </Masonry>
+          </ResponsiveMasonry>
+        )}
       </div>
     </div>
   );
 }
+
+const SelectCategory = ({
+  value,
+  onChange,
+  categories,
+}: {
+  value: string;
+  onChange: (s: string) => void;
+  categories: string[];
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const handleOnClick = (e: MouseEvent) => {
+    if (buttonRef.current && !buttonRef.current.contains(e.target as Node)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", handleOnClick);
+
+    return () => window.removeEventListener("click", handleOnClick);
+  }, []);
+
+  return (
+    <div className="relative">
+      <button
+        className="w-[180px] border-2 border-border bg-foreground py-1 text-lg dark:border-dark-border dark:bg-transparent"
+        ref={buttonRef}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {value || "Select category"}
+      </button>
+      {isOpen && (
+        <div className="absolute top-full w-full divide-y-2 divide-border border-2 border-border bg-foreground dark:divide-dark-border dark:border-dark-border dark:bg-dark-foreground">
+          <button
+            className="block w-full p-2 text-left"
+            onClick={() => onChange("")}
+          >
+            all
+          </button>
+          {categories.map((category) => (
+            <button
+              onClick={() => onChange(category)}
+              className="block w-full p-2 text-left"
+            >
+              {category}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default ProductsPage;
