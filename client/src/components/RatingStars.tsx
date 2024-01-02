0fc9616e38b8e179b921935445eb387e84e0dc81
v1st78 <petrenkov605@gmail.com>
2024-01-02 13:46:10 +0200

    add remainder RatingStars

--- client/src/components/RatingStars.tsx
+++ client/src/components/RatingStars.tsx
@@ -3,6 +3,7 @@ import { StarIcon } from "@heroicons/react/24/solid";
 
 export default function RatingStars({ value }: { value: number }) {
   const filledStars = Math.floor(value);
+  const remainder = value - filledStars;
 
   return (
     <div className="flex">
@@ -10,7 +11,25 @@ export default function RatingStars({ value }: { value: number }) {
         <StarIcon className="h-6 w-6 text-yellow-500" key={i} />
       ))}
 
-      {Array.from({ length: 5 - filledStars }, (_, i) => i).map((i) => (
+      {remainder !== 0 && (
+        <div className="relative h-6 w-6">
+          <div
+            style={{
+              width: `${remainder * 100}%`,
+            }}
+            className="absolute h-6 overflow-hidden"
+          >
+            <StarIcon className="h-6 w-6 text-yellow-500" />
+          </div>
+
+          <EmptyStarIcon className="h-6 w-6 text-yellow-500" />
+        </div>
+      )}
+
+      {Array.from(
+        { length: 5 - filledStars - (remainder !== 0 ? 1 : 0) },
+        (_, i) => i,
+      ).map((i) => (
         <EmptyStarIcon className="h-6 w-6 text-yellow-500" key={i} />
       ))}
     </div>
