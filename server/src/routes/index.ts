02db408091906ec06b50edfabbdeba80d05a3667
v1st78 <petrenkov605@gmail.com>
2024-01-07 19:58:32 +0200

    add userRouter and /changepassword

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import authRouter from "./authRouter";
+import userRouter from "./userRouter";
+import { requireAuth } from "../middleware/authMiddleware";
 
-const apiRouter = Router().use("/auth", authRouter);
+const apiRouter = Router()
+  .use("/auth", authRouter)
+  .use("/user", requireAuth, userRouter);
 
 export default apiRouter;
---
+++ server/src/routes/userRouter.ts
@@ -0,0 +1,30 @@
+import { Response, Router } from "express";
+import { UserRequest } from "../types/user";
+import hashPassword from "../utils/hashPassword";
+import query from "../utils/db";
+
+const userRouter = Router().post(
+  "/changepassword",
+  async (req: UserRequest, res: Response) => {
+    try {
+      if (!req.user) return res.status(401);
+
+      const { password: newPassword } = req.body;
+      if (!newPassword) return res.status(400);
+
+      const hashedNewPassword = await hashPassword(newPassword);
+      console.log(req.user);
+
+      await query("UPDATE users SET password = ? WHERE id = ?", [
+        hashedNewPassword,
+        req.user.id,
+      ]);
+
+      return res.status(200).end();
+    } catch (e) {
+      return res.status(500).end();
+    }
+  }
+);
+
+export default userRouter;
