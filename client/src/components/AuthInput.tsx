bfa24a23253e49c6ed472eac6d23428695c7fbf2
v1st78 <petrenkov605@gmail.com>
2024-01-10 19:24:46 +0200

    fix eye position when error AuthInput

--- client/src/components/AuthInput.tsx
+++ client/src/components/AuthInput.tsx
@@ -29,32 +29,34 @@ function AuthInput({
     : "border-border hover:border-primary focus:border-primary dark:border-dark-border";
 
   return (
-    <div className={`mb-${mb} ${type === "password" && "relative"}`}>
+    <div className={`mb-${mb} `}>
       <label htmlFor={name} className="mb-1 block">
         {label}{" "}
         <span className="text-sm text-error">{isError && errorMessage}</span>
       </label>
-      <input
-        name={name}
-        value={value}
-        onChange={(e) => onChange && onChange(e.target.value)}
-        className={`mb-2 w-full border-2 p-2 outline-none dark:text-copy ${borderStyle}`}
-        type={type !== "password" ? type : show ? "text" : "password"}
-        required={required}
-      />
-      {type === "password" && (
-        <button
-          onClick={() => setShow(!show)}
-          className="absolute right-3 top-1/2"
-          type="button"
-        >
-          {show ? (
-            <EyeSlashIcon className="h-6 w-6 text-copy" />
-          ) : (
-            <EyeIcon className="h-6 w-6 text-copy" />
-          )}
-        </button>
-      )}
+      <div className={`${type === "password" && "relative"} h-[44px]`}>
+        <input
+          name={name}
+          value={value}
+          onChange={(e) => onChange && onChange(e.target.value)}
+          className={`mb-2 w-full border-2 p-2 outline-none dark:text-copy ${borderStyle}`}
+          type={type !== "password" ? type : show ? "text" : "password"}
+          required={required}
+        />
+        {type === "password" && (
+          <button
+            onClick={() => setShow(!show)}
+            className="absolute right-3 top-1/2 -translate-y-1/2 transform"
+            type="button"
+          >
+            {show ? (
+              <EyeSlashIcon className="h-6 w-6 text-copy" />
+            ) : (
+              <EyeIcon className="h-6 w-6 text-copy" />
+            )}
+          </button>
+        )}
+      </div>
     </div>
   );
 }
