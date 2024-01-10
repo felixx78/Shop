04862cf5d3f847ec8dceded5b9a46dc4d2eb13e2
v1st78 <petrenkov605@gmail.com>
2024-01-10 19:18:18 +0200

    refactor LoginPage

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -20,8 +20,8 @@ function LoginPage() {
   });
 
   const handleOnSubmit = () => {
-    const email = data.email.toString().trim();
-    const password = data.password.toString().trim();
+    const email = data.email.trim();
+    const password = data.password.trim();
 
     if (!email) {
       setErrors((prev) => ({
