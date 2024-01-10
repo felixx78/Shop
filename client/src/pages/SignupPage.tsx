a9d486f1357f09be7de0cbfcde5b2cc2a4faa8e6
v1st78 <petrenkov605@gmail.com>
2024-01-10 19:29:51 +0200

    refactor SignupPage

--- client/src/pages/SignupPage.tsx
+++ client/src/pages/SignupPage.tsx
@@ -4,27 +4,27 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "../api/axios";
 import { userActions } from "../reducer/userReducer";
 import AuthInput from "../components/AuthInput";
+import SubmitButton from "../components/SubmitButton";
 
 function SignupPage() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+    confirm_password: "",
+  });
   const [errors, setErrors] = useState({
     email: { isError: false, errorMessage: "" },
     password: { isError: false, errorMessage: "" },
     confirm_password: { isError: false, errorMessage: "" },
   });
 
-  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const formData = new FormData(e.currentTarget);
-    const email = formData.get("email")?.toString().trim();
-    const password = formData.get("password")?.toString().trim();
-    const confirm_password = formData
-      .get("confirm_password")
-      ?.toString()
-      .trim();
+  const handleOnSubmit = () => {
+    const email = data.email.trim();
+    const password = data.password.trim();
+    const confirm_password = data.confirm_password.trim();
 
     if (!email) {
       setErrors((prev) => ({
@@ -97,16 +97,15 @@ function SignupPage() {
   };
   return (
     <div className="flex flex-col justify-center px-2 pt-6 md:min-h-[70vh]">
-      <form
-        onSubmit={handleOnSubmit}
-        className="mx-auto rounded-md bg-foreground px-6 py-8 sm:w-[400px] dark:bg-dark-foreground"
-      >
+      <div className="mx-auto rounded-md bg-foreground px-6 py-8 sm:w-[400px] dark:bg-dark-foreground">
         <h1 className="mb-2 text-center text-2xl font-bold">Sign Up</h1>
 
         <AuthInput
           label="Email"
           name="email"
           type="email"
+          value={data.email}
+          onChange={(s) => setData((prev) => ({ ...prev, email: s }))}
           isError={errors.email.isError}
           required
           errorMessage={errors.email.errorMessage}
@@ -116,6 +115,8 @@ function SignupPage() {
           label="Password"
           name="password"
           type="password"
+          value={data.password}
+          onChange={(s) => setData((prev) => ({ ...prev, password: s }))}
           isError={errors.password.isError}
           required
           errorMessage={errors.password.errorMessage}
@@ -125,18 +126,17 @@ function SignupPage() {
           label="Confirm Password"
           name="confirm_password"
           type="password"
+          value={data.confirm_password}
+          onChange={(s) =>
+            setData((prev) => ({ ...prev, confirm_password: s }))
+          }
           isError={errors.confirm_password.isError}
           errorMessage={errors.confirm_password.errorMessage}
           required
           mb="4"
         />
 
-        <button
-          type="submit"
-          className="mb-4 block w-full bg-primary py-2 text-dark-copy hover:bg-primary-dark"
-        >
-          Sign up
-        </button>
+        <SubmitButton onClick={handleOnSubmit} className="mb-4" />
 
         <div className="text-copy-light dark:text-dark-copy-light">
           Have already an account?{" "}
@@ -144,7 +144,7 @@ function SignupPage() {
             Login
           </Link>
         </div>
-      </form>
+      </div>
     </div>
   );
 }
