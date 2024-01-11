29f18f63816b3abaf06e0a8f8bf3482040a29084
v1st78 <petrenkov605@gmail.com>
2024-01-12 01:14:33 +0200

    add /awake to server

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,20 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import authRouter from "./authRouter";
 import userRouter from "./userRouter";
 import { requireAuth } from "../middleware/authMiddleware";
+import query from "../utils/db";
 
 const apiRouter = Router()
   .use("/auth", authRouter)
-  .use("/user", requireAuth, userRouter);
+  .use("/user", requireAuth, userRouter)
+  .get("/awake", async (req: Request, res: Response) => {
+    try {
+      await query("SELECT * from users");
+      return res.status(200).end();
+    } catch (e) {
+      console.log(e);
+      return res.status(500).end();
+    }
+  });
 
 export default apiRouter;
