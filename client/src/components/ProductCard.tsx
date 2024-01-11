9e2eb2a42ea05ad76ed73c30d3ba597195517b58
v1st78 <petrenkov605@gmail.com>
2024-01-11 03:24:46 +0200

    refactor ProductCard

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -1,92 +1,50 @@
 import { Link } from "react-router-dom";
-import RatingStars from "./RatingStars";
-import { Product, RootState } from "../lib/definition";
+import { Product } from "../lib/definition";
 import Skeleton from "react-loading-skeleton";
 import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { cartActions } from "../reducer/cartReducer";
-import { CheckIcon } from "@heroicons/react/24/outline";
 
 const ProductCard = ({ product }: { product: Product }) => {
-  const cartItems = useSelector((state: RootState) => state.cart.items);
-  const dispatch = useDispatch();
-
   const [isImageLoaded, setIsImageLoaded] = useState(false);
-  const [isAdded, setIsAdded] = useState(
-    !!cartItems.find((i) => i.productId === product.id),
-  );
-
-  const addToCart = () => {
-    if (isAdded) return;
-
-    dispatch(cartActions.addItem(product.id));
-    setIsAdded(true);
-  };
 
   return (
-    <div className="mx-auto w-[290px] bg-foreground pt-4 dark:bg-dark-foreground">
-      <Link to={`/product/${product.id}`} className="mb-4 block px-4">
-        {!isImageLoaded && <Skeleton height={350} className="mb-2 w-full" />}
+    <div className="mx-auto w-[290px] bg-white pt-4">
+      <Link to={`/product/${product.id}`} className="block">
+        {!isImageLoaded && (
+          <Skeleton height={280} className="w-full" borderRadius={0} />
+        )}
 
         <img
           src={product.image}
-          className={`mb-2 text-center ${
+          className={`mx-auto h-[280px] w-full object-contain p-8 text-center ${
             isImageLoaded ? "visible" : "invisible absolute h-0 w-0"
           }`}
           alt=""
           onLoad={() => setIsImageLoaded(true)}
         />
 
-        <p className="mb-2 truncate text-lg font-bold" title={product.title}>
-          {product.title}
-        </p>
-
-        <div className="flex items-center justify-between">
-          <p>{product.price}$</p>
-          <RatingStars value={product.rating.rate} />
+        <div className="w-full bg-dark-foreground p-1 px-2">
+          <p className="">{product.price}$</p>
+          <p className="truncate text-lg" title={product.title}>
+            {product.title}
+          </p>
         </div>
       </Link>
-
-      <button
-        onClick={addToCart}
-        className={`flex w-full items-center justify-center gap-2 py-2 text-lg text-dark-copy ${
-          isAdded
-            ? "cursor-default bg-success"
-            : "bg-primary hover:bg-primary-dark"
-        }`}
-      >
-        {isAdded ? (
-          <>
-            <span>Added</span>
-            <CheckIcon className="h-6 w-6" />
-          </>
-        ) : (
-          "Add to cart"
-        )}
-      </button>
     </div>
   );
 };
 
 export const ProductCardSkeleton = () => {
   return (
-    <div className="mx-auto mb-4 w-[290px] bg-foreground pt-4 dark:bg-dark-foreground">
-      <div className="mb-4 px-4">
-        {/* image */}
-        <Skeleton height={350} className="mb-2 w-full" />
+    <div className="mx-auto w-[290px]">
+      {/* image */}
+      <Skeleton height={280} className="w-full bg-white" borderRadius={0} />
 
+      <div className="w-full bg-dark-foreground p-1 px-2">
+        {/* price */}
+        <Skeleton width={60} height={15} />
         {/* title */}
-        <Skeleton width={140} height={25} className="mb-2" />
-
-        {/* price and stars */}
-        <div className="flex items-center justify-between">
-          <Skeleton width={80} height={20} />
-          <Skeleton width={100} height={20} />
-        </div>
+        <Skeleton width={170} height={18} />
       </div>
-
-      {/* button  */}
-      <Skeleton height={40} className="w-full" />
     </div>
   );
 };
