53311b5eb021de68b59e04907f783bff3159be72
v1st78 <petrenkov605@gmail.com>
2024-04-08 19:24:04 +0300

    fix AllowCookie width

--- client/src/components/AllowCookie.tsx
+++ client/src/components/AllowCookie.tsx
@@ -15,15 +15,19 @@ function AllowCookie() {
     <div
       className={`${
         isHidden ? "hidden" : ""
-      } absolute left-0 top-0 z-50 flex min-h-[70px] items-start bg-secondary px-4 py-2 text-dark-copy`}
+      } absolute left-0 top-0 z-50 flex min-h-[70px] w-full items-start bg-secondary px-4 py-2 text-dark-copy`}
     >
-      <p className="px-2">
-        If you're using this site, you're allowing the use of cookies. Cookies
-        are small text files that are stored on your device to enhance your
-        browsing experience. By continuing to use this site, you consent to the
-        use of cookies
-      </p>
-      <button onClick={handleClose} className="cursor-pointer">
+      <div className="px-2">
+        <p className="pr-6 lg:inline lg:pr-2">
+          If you're using this site, you're allowing the use of cookies. Cookies
+          are small text files that are stored on your device to enhance your
+        </p>
+        <p>
+          browsing experience. By continuing to use this site, you consent to
+          the use of cookies
+        </p>
+      </div>
+      <button onClick={handleClose} className="absolute right-4 cursor-pointer">
         <XMarkIcon className="h-8 w-8" />
       </button>
     </div>
